8e16157e009ef794d44b3548e16f593953ecadc6
Kyle <kyle.hickey222@gmail.com>
2018-02-13 07:02:45 -0600

    Make linux data directory hidden

--- DownloaderForReddit/Core/SystemUtil.py
+++ DownloaderForReddit/Core/SystemUtil.py
@@ -74,7 +74,7 @@ def get_data_directory():
     """
     data_dir = os.path.join('SomeGuySoftware', 'DownloaderForReddit')
     if sys.platform == 'win32':
-        path = os.path.join(os.getenv('APPDATA'), data_dir)
+        path = os.path.join(os.getenv('APPDATA'), '.%s' % data_dir)
     elif sys.platform.startswith('linux'):
         path = os.path.join(os.path.expanduser('~'), data_dir)
     elif sys.platform == 'darwin':
