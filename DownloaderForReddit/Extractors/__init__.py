2fcc6843b2a8d7a869e3e1c726635b2fc8de4038
Kyle <kyle.hickey222@gmail.com>
2018-03-02 11:50:41 -0600

    Remove debugging item

--- DownloaderForReddit/Extractors/__init__.py
+++ DownloaderForReddit/Extractors/__init__.py
@@ -33,6 +33,5 @@ sys.modules['Post'] = Post
 # Imports each extractor module in the Extractor package.  This makes it possible to dynamically search each
 # BaseExtractor subclass during operation.
 path = pkgutil.extend_path(__path__, __name__)
-print('__path__: %s\npath: %s\n__name__: %s' % (__path__, path, __name__))
 for importer, name, ispkg in pkgutil.walk_packages(path=path, prefix=__name__ + '.'):
     __import__(name)
