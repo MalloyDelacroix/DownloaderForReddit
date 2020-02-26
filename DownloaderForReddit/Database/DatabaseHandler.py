661a223405fd0e85b1cf0771ecc36b1501e39666
Kyle <kyle.hickey222@gmail.com>
2020-02-26 15:04:27 -0600

    Add generic get_or_create method

--- DownloaderForReddit/Database/DatabaseHandler.py
+++ DownloaderForReddit/Database/DatabaseHandler.py
@@ -67,3 +67,33 @@ class DatabaseHandler:
 
     def commit_object(self, obj):
         self.get_object_session(obj).commit()
+
+    def get_or_create(self, model, session=None, defaults=None, **kwargs):
+        """
+        Queries the supplied model by the supplied kwargs and returns the model instance if one is found.  If a matching
+        model instance is not found, one is created in the database and then returned.  Returns a tuple, the second part
+        of which is a bool indicating if the instance was created or not.
+        :param model: The model that is to be queried or created.
+        :param session: An optional session instance.  If not supplied, a default scoped session is used.
+        :param defaults: A dict of default values to be used in a new model instances creation.
+        :param kwargs: Model attributes with matching values that are to be queried and if not found, used to create a
+                       new model instance.
+        :return: A tuple containing the instance that is found or created and a bool indicating if the instance was
+                 created or not respectively.
+        :rtype: tuple
+        """
+        if session is None:
+            with self.get_scoped_session() as session:
+                return self.get_or_create(model, session=session, defaults=defaults, **kwargs)
+        instance = session.query(model).filter_by(**kwargs).first()
+        if instance:
+            return instance, False
+        else:
+            params = {}
+            if defaults is not None:
+                params.update(defaults)
+            params.update(kwargs)
+            instance = model(**params)
+            session.add(instance)
+            session.commit()
+            return instance, True
