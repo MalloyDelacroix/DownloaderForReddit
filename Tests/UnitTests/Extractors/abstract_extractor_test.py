9ce0e89707d793bcff6800af25f963441efed339
Kyle <kyle.hickey222@gmail.com>
2020-07-02 14:58:49 -0500

    Fix gfycat tests

--- Tests/UnitTests/Extractors/abstract_extractor_test.py
+++ Tests/UnitTests/Extractors/abstract_extractor_test.py
@@ -9,12 +9,21 @@ class ExtractorTest(TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.db = DatabaseHandler(in_memory=True)
         cls.settings = MagicMock()
         injector.settings_manager = cls.settings
 
     def setUp(self):
+        self.db = DatabaseHandler(in_memory=True)
         self.session = self.db.get_session()
 
     def tearDown(self):
         self.session.close()
+
+    def check_output(self, extractor, url, post):
+        content = extractor.extracted_content[0]
+        self.assertEqual(url, content.url)
+        self.assertEqual(post.title, content.title)
+        self.assertEqual(post, content.post)
+        self.assertEqual(post.author, content.user)
+        self.assertEqual(post.subreddit, content.subreddit)
+        self.assertIsNotNone(content.id)
