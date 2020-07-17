7de194ca7f759a17fefec4ad85fc6a4ad9325c66
Kyle <kyle.hickey222@gmail.com>
2020-07-17 08:08:57 -0500

    Fix reddit object type writing

--- DownloaderForReddit/utils/exporters/csv_exporter.py
+++ DownloaderForReddit/utils/exporters/csv_exporter.py
@@ -3,6 +3,7 @@ import csv
 import sqlite3
 
 from DownloaderForReddit.database.database_handler import DatabaseHandler
+from DownloaderForReddit.database.models import RedditObject
 
 
 logger = logging.getLogger(f'DownloaderForReddit.{__name__}')
@@ -10,10 +11,14 @@ logger = logging.getLogger(f'DownloaderForReddit.{__name__}')
 
 def export_csv(object_list, model, file_path):
     table = model.__tablename__
+    if model != RedditObject:
+        model_type = model.__name__
+    else:
+        model_type = object_list[0].object_type.title()
     id_list = [x.id for x in object_list]
     con = sqlite3.connect(DatabaseHandler.database_path)
     with open(file_path, 'w', newline='', encoding='utf-8') as file:
-        file.write(model.__name__ + '\n')
+        file.write(model_type + '\n')
         writer = csv.writer(file)
         cursor = con.cursor()
         cursor.execute(f'SELECT * FROM {table} WHERE {table}.id in ({",".join(["?"] * len(id_list))})', id_list)
