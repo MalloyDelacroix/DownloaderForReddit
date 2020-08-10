c9a531a9ad516b58800708ede7407bf222b6b70c
Kyle <kyle.hickey222@gmail.com>
2020-08-10 06:46:22 -0500

    Handle exception when unable to open in system

--- DownloaderForReddit/utils/system_util.py
+++ DownloaderForReddit/utils/system_util.py
@@ -50,11 +50,14 @@ def open_in_system(item):
     :param item: A full path to a file system item that can be opened.
     :type item: str
     """
-    if sys.platform == 'win32':
-        os.startfile(item)
-    else:
-        opener = 'open' if sys.platform == 'darwin' else 'xdg-open'
-        subprocess.call([opener, item])
+    try:
+        if sys.platform == 'win32':
+            os.startfile(item)
+        else:
+            opener = 'open' if sys.platform == 'darwin' else 'xdg-open'
+            subprocess.call([opener, item])
+    except:
+        logger.error('Failed to open in system', extra={'item_url': item}, exc_info=True)
 
 
 def clean_path(path):
