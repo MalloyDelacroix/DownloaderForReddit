c117e15078ed0a63fa1bd41d69a901a1ee3a7d59
Kyle <kyle.hickey222@gmail.com>
2021-10-21 07:00:06 -0500

    Fix database file not being created directory specified with cli command

--- DownloaderForReddit/utils/exporters/csv_exporter.py
+++ DownloaderForReddit/utils/exporters/csv_exporter.py
@@ -2,21 +2,22 @@ import logging
 import csv
 import sqlite3
 
-from DownloaderForReddit.database.database_handler import DatabaseHandler
 from DownloaderForReddit.database.models import RedditObject, RedditObjectList
+from DownloaderForReddit.utils import injector
 
 
 logger = logging.getLogger(f'DownloaderForReddit.{__name__}')
 
 
 def export_csv(object_list, model, file_path):
+    database_path = injector.get_database_handler().database_path
     table = model.__tablename__
     if model != RedditObject:
         model_type = model.__name__
     else:
         model_type = object_list[0].object_type.title()
     id_list = get_id_list(object_list)
-    con = sqlite3.connect(DatabaseHandler.database_path)
+    con = sqlite3.connect(database_path)
     with open(file_path, 'w', newline='', encoding='utf-8') as file:
         file.write(model_type + '\n')
         writer = csv.writer(file)
